c28f022fb9b9ec2ce04d5ca9958a7b322f3d32ff
subhalaxmi-srichandan08 <subhalaxmisrichandan08@gmail.com>
2026-07-15 09:28:22 +0530

    fix: cookie issue

--- src/services/authService.js
+++ src/services/authService.js
@@ -170,7 +170,7 @@ class AuthService {
 
         secure: process.env.NODE_ENV === "production",
 
-        sameSite: "lax",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
 
         maxAge: 7 * 24 * 60 * 60 * 1000
 
