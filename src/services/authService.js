6fdb1ae0f05ccafc021358da224bfca9b89800f8
subhalaxmi-srichandan08 <subhalaxmisrichandan08@gmail.com>
2026-07-15 09:48:50 +0530

    fix: cookie

--- src/services/authService.js
+++ src/services/authService.js
@@ -92,7 +92,7 @@ class AuthService {
     async refresh(req, res) {
 
         const refreshToken = req.cookies.refreshToken;
-        console.log("Cookies:", req.cookies.toString());
+        console.log("Cookies:", JSON.stringify(req.cookies,null,2));
         if (!refreshToken) throw new ApiError(401, "Unauthorized.");
 
         let payload;
